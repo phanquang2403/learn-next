702218e5be6dfdf1ba3f9baae36b960ec4c1063b
phanquang <phandangquang2403@gmail.com>
2023-05-11 17:04:19 +0700

    SSG - data fecth on client

---
+++ src/component/Header.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Header = () => {
+  console.log("render _________");
+  return <div>Header</div>;
+};
+
+export default Header;
--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import dynamic from "next/dynamic";
+import React, { Component, FunctionComponent, ReactElement } from "react";
 
-const about = () => {
-  return <div>about</div>;
+interface Props {
+  posts?: {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+  }[];
+}
+
+//sss = true thì sẽ client serer
+// nếu k có dynamic thì sẽ render cả client cả server
+const Header: any = dynamic(() => import("@/component/Header"), {
+  ssr: false,
+});
+
+const AboutPage = () => {
+  return (
+    <>
+      <h1>About</h1>
+      <Header />;
+    </>
+  );
 };
 
-export default about;
+export default AboutPage;
+
+export const getServerSideProps = async () => {
+  return {
+    props: {},
+  };
+};
